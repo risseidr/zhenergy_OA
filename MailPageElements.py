becff17df39df25ea938fd5732a0e70c3d11e6a8
risseidr <binyuan.zhu@gmail.com>
2020-03-08 19:06:18 +0800

    20200308

--- MailPageElements.py
+++ MailPageElements.py
@@ -14,14 +14,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.ie.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from datetime import datetime
-import time
+from time import sleep
+from win32api import SendMessage
+from win32gui import FindWindow, FindWindowEx
+from win32con import WM_KEYUP, WM_KEYDOWN, VK_RETURN
 
 
 def format_time(str_time: str):
-    if 'AM' in str_time or 'PM' in str_time:
-        return datetime.strptime(str_time, '%m/%d/%Y %I:%M:%S %p')
-    else:
-        return datetime.strptime(str_time, '%Y/%m/%d %H:%M')
+    if len(str_time) == 19:
+        return datetime.strptime(str_time, '%Y-%m-%d %H:%M:%S')
+    elif len(str_time) == 16:
+        return datetime.strptime(str_time, '%Y-%m-%d %H:%M')
 
 
 class BriefMailPageElements(object):
@@ -36,6 +39,7 @@ class BriefMailPageElements(object):
     def next_page(self):
         if 'page_ok' in str(self._next_page_button.get_attribute('class')):
             self._next_page_button.click()
+            sleep(0.5)
             return True
         else:
             return False
@@ -43,6 +47,7 @@ class BriefMailPageElements(object):
     def pre_page(self):
         if 'page_ok' in str(self._pre_page_button.get_attribute('class')):
             self._pre_page_button.click()
+            sleep(0.5)
             return True
         else:
             return False
@@ -53,57 +58,80 @@ class BriefMailPageElements(object):
 
 class BriefInfo(object):
     def __init__(self, tr: WebElement):
-        # if tr.find_element_by_xpath('.//td[2]').get_attribute('title') == 'Mail.gif':
-        #     self._read_flag = True
-        # else:
-        #     self._read_flag = False
-        self._checkbox = tr.find_element_by_xpath('.//td[1]/input')
-        # self._from = tr.find_element_by_xpath('.//td[3]').get_attribute('title')
-        self._subject = tr.find_element_by_xpath('.//td[5]').get_attribute('title')
-        # self._time = format_time(tr.find_element_by_xpath('.//td[6]').get_attribute('title'))
-        # if tr.find_element_by_xpath('.//td[7]').get_attribute('title') == 'Attachment.gif':
-        #     self._attachment_flag = True
-        # else:
-        #     self._attachment_flag = False
+        """
+        must sent tr
+
+        :param tr:
+        """
+        self._td_ = tr.find_elements_by_xpath('.//td')
+        if 'read_flag' in tr.get_attribute('class'):
+            self._read_flag = False
+        else:
+            self._read_flag = True
+        self._checkbox = ''
+        self._from = ''
+        self._subject = ''
+        if self._td_[6].get_attribute('title') == 'Attachment.gif':
+            self._attachment_flag = True
+        else:
+            self._attachment_flag = False
+        self._time = datetime.now()
 
     def open(self):
-        self._subject.click()
+        sleep(0.1)
+        self._td_[4].click()
+        sleep(0.5)
 
-    def set_del_tag(self):
+    def choose(self):
+        self._checkbox = self._td_[0].find_element_by_xpath('.//input')
         if not (self._checkbox.is_selected()):
+            sleep(0.1)
             self._checkbox.click()
+            sleep(0.1)
 
+    @property
     def read_flag(self):
         return self._read_flag
 
+    @property
     def mail_from(self) -> str:
+        self._from = self._td_[2].text
         return self._from
 
+    @property
     def subject(self) -> str:
+        self._subject = self._td_[4].text
         return self._subject
 
+    @property
     def time(self):
+        self._time = format_time(self._td_[5].text)
         return self._time
 
+    @property
     def attachment_flag(self):
         return self._attachment_flag
 
 
 class SingleMailPageElements(object):
-    def __init__(self, driver: WebDriver, attachment_flag: bool):
-        driver.implicitly_wait(10)
-        self._close_button = driver.find_element_by_css_selector(
+    def __init__(self, driver: WebDriver, attachment_flag: bool = True):
+        self._mail_driver = driver
+        self._mail_driver.implicitly_wait(3)
+        mail_frame = self._mail_driver.find_element_by_css_selector(
+            'div.J_iframe[style="display: block;"] > div > div > iframe')
+        self._mail_driver.switch_to.frame(mail_frame)
+        self._mail_driver.switch_to.active_element
+        self._close_button = self._mail_driver.find_element_by_css_selector(
             'html > body > form > div:nth-of-type(3) > div > a:nth-of-type(1)')
         if attachment_flag:
-            self._download_button = driver.find_element_by_css_selector('a#dfAll')
-
-        self._from = driver.find_element_by_css_selector(
+            self._download_button = self._mail_driver.find_element_by_css_selector('a#dfAll')
+        self._from = self._mail_driver.find_element_by_css_selector(
             'html > body > form > div.row-container > table > tbody > tr:nth-child(1) > td:nth-child(2) > '
             'font:nth-child(1)').text
-        self._subject = driver.find_element_by_css_selector(
-            'body > form > div.row-container > table > tbody > tr:nth-child(4) > td:nth-child(2) > b').text
-        self._mail = driver.find_element_by_css_selector('div.row-container').get_attribute('innerHTML')
-        self._time = format_time(driver.find_element_by_css_selector(
+        self._subject = self._mail_driver.find_element_by_css_selector(
+            'body > form > div.row-container > table > tbody > tr:nth-child(3) > td:nth-child(2) > b').text
+        self._mail = self._mail_driver.find_element_by_css_selector('div.row-container').get_attribute('innerHTML')
+        self._time = format_time(self._mail_driver.find_element_by_css_selector(
             'html > body > form > div.row-container > table > tbody > tr:nth-child(1) > td:nth-child(2) > '
             'font:nth-child(4)').text)
 
@@ -112,9 +140,15 @@ class SingleMailPageElements(object):
         return True
 
     def download_all(self):
+        sleep(0.2)
         self._download_button.click()
-        time.sleep(0.5)
-        self._download_button.send_keys(Keys.ENTER)
+        sleep(0.2)
+        hwnd = FindWindow(0, "确定")
+        SendMessage(hwnd, WM_KEYDOWN, VK_RETURN, 0)
+        SendMessage(hwnd, WM_KEYUP, VK_RETURN, 0)
+        sleep(0.2)
+
+
 
     def mail_from(self):
         return self._from
